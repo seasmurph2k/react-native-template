1c12244a54c9c33f8bc25a15c560d2f5b06e4dc2
Connor Murphy <iam@connormurphy.co.uk>
2023-05-05 16:36:15 +0100

    Basic app bar

--- src/nav/HomeNav.js
+++ src/nav/HomeNav.js
@@ -1,12 +1,16 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
-
+import AppBar from '../AppBar';
 const HomeStack = createNativeStackNavigator();
 
 export default function HomeNav() {
   return (
-    <HomeStack.Navigator initialRouteName="Home">
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        header: props => <AppBar {...props} />,
+      }}>
       <HomeStack.Screen name="Home" component={Home} />
       <HomeStack.Screen name="Profile" component={Profile} />
     </HomeStack.Navigator>
